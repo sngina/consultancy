ef3de956c0a049010ad04b09cddb8829e706c960
sngina <sngina707@gmail.com>
2021-06-19 10:51:29 +0300

    install werkzeug

---
+++ App/models.py
@@ -0,0 +1,6 @@
+from . import db
+from sqlalchemy.sql import func
+from . import login_manager
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+
