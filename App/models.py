50586eaaff21e10e31c6f607a1588a0227ec3db2
sngina <sngina707@gmail.com>
2021-06-20 12:15:51 +0300

    add class user in the model.py

--- App/models.py
+++ App/models.py
@@ -16,4 +16,19 @@ class User(UserMixin ,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
-    pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    blog = db.relationship('block', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'{self.username}'
+
