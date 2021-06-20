8313611854bfc7b7bb62ac7c8589fea3ebd5e400
sngina <sngina707@gmail.com>
2021-06-20 16:31:20 +0300

    downgrade Flask

--- manage.py
+++ manage.py
@@ -1,7 +1,29 @@
 from app import create_app ,db
-from app.models import User
-from flask_script import Manager ,Serever
+from flask_script import Manager ,Server 
 from flask_migrate import Migrate ,MigrateCommand
-
+#app instances
 app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app ,db)
+manager.add_default_commands('db' ,MigrateCommand)
+
+
+@manager.command
+def test():
+
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db=db)
+
+if __name__ == '__main__':
+    manager.run()
+
+
 
