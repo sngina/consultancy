c36c6a5083312d8d63b6551187b84ac42203283c
sngina <sngina707@gmail.com>
2021-06-20 13:43:05 +0300

    add main index in the view

--- App/main/views.py
+++ App/main/views.py
@@ -8,3 +8,23 @@ from .. import db
 from .forms import BlogForm , CommentForm
 
 
+@main.route('/')
+def index():
+    blog = Blog.get_blogs()
+    title = 'Home'
+
+    return render_template('home.html' , title = title)
+
+
+@main.route('/pitches/new/', methods = ['GET','POST'])
+@login_required
+def new_blog():
+    form = BlogForm()
+
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        owner_id = current_user
+        new_blog = Blog(owner_id = current_user._get_current_object().id,title = title , description = description)
+        db.session.add(new_blog)
+        db.session.commit()
